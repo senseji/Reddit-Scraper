a19f8070b6a07f4ede45a7d40541111d11cba0e2
senseji <34347036+senseji@users.noreply.github.com>
2021-07-10 11:13:22 +0200

    Delete reddit_settings (copy).py

--- reddit_settings (copy).py
+++
@@ -1,9 +0,0 @@
-import praw
-
-def getReddit():
-
-    reddit = praw.Reddit(
-        client_id='yezlsT_S-LplPbDOT8Q9rw', 
-        client_secret='fS8KXBfnhFTPDJw74oXriq2vi2_GrA', 
-        user_agent='SCRAPER')
-    return reddit
